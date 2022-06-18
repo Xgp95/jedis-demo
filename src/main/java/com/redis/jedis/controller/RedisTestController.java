29cf409739dba4504a4c1aed04d4a16405b64e0e
Xugp <79002319@qq.com>
2022-06-18 17:49:02 +0800

    Initial commit

---
+++ src/main/java/com/redis/jedis/controller/RedisTestController.java
@@ -0,0 +1,27 @@
+package com.redis.jedis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author: Xugp
+ * @date: 2022/6/17 15:43
+ * @description:
+ */
+@Controller
+public class RedisTestController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping(value = "/redisTest")
+    @ResponseBody
+    public String redisTest() {
+        redisTemplate.opsForValue().set("name", "jack");
+        String name = (String) redisTemplate.opsForValue().get("name");
+        System.out.println(redisTemplate);
+        return name;
+    }
+}
---
+++ src/test/java/com/redis/jedis/JedisApplicationTests.java
@@ -0,0 +1,93 @@
+package com.redis.jedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+//@SpringBootTest
+class JedisApplicationTests {
+
+    Jedis jedis = new Jedis("172.16.0.107", 6379);
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+//    @Disabled
+    void jedisTest() {
+
+        String pong = jedis.ping();
+        System.out.println(pong);
+    }
+
+    @Test
+    void keyTest() {
+        jedis.mset("k1","v1","k2","v2","k3","1");
+        Set<String> keys = jedis.keys("*");
+        for (String key : keys) {
+            jedis.sadd("keys", key);
+//            System.out.println(key);
+        }
+        Set<String> set = jedis.smembers("keys");
+        for (String s : set) {
+            System.out.println(s);
+        }
+        System.out.println("@@@@@@@");
+        jedis.incrBy("k3",10);
+        System.out.println(jedis.get("k3"));
+        System.out.println("~~~~~~~~~~~~~~");
+        jedis.rpush("k4:list", "a","b","c","d");
+        List<String> lrange = jedis.lrange("k4:list", 0, -1);
+        for (String s : lrange) {
+            System.out.print(s + " ");
+        }
+        jedis.unlink("k4:list");
+        System.out.println();
+        System.out.println("~~~~~~~~~~~~~~");
+        jedis.sadd("k5:set", "a","b","c","d","a","aa");
+        Set<String> smembers = jedis.smembers("k5:set");
+        for (String s : smembers) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        System.out.println("~~~~~~~~~~~~~~");
+        jedis.hset("user:101", "id", "001");
+        jedis.hset("user:101", "name", "zhangsan");
+        jedis.hset("user:101", "age", "18");
+        Map<String, String> map = new HashMap<>();
+        map.put("id", "002");
+        map.put("name", "lisi");
+        map.put("age", "20");
+        jedis.hmset("user:102", map);
+        List<String> hvals101 = jedis.hvals("user:101");
+        for (String s : hvals101) {
+            System.out.print(s + " ");
+        }
+        System.out.println("\n!!!!");
+        List<String> hvals102 = jedis.hvals("user:102");
+        for (String s : hvals102) {
+            System.out.print(s + " ");
+        }
+        System.out.println("\n!!!!!!!!!");
+        jedis.zadd("k6:zset", 10, "c");
+        jedis.zadd("k6:zset", 10, "c");
+        jedis.zadd("k6:zset", 10, "c++");
+        jedis.zadd("k6:zset", 100, "php");
+        jedis.zadd("k6:zset", 50, "java");
+        Set<Tuple> tuples = jedis.zrangeByScoreWithScores("k6:zset", 0, 100);
+        for (Tuple tuple : tuples) {
+            System.out.println(tuple);
+        }
+        jedis.flushDB();
+        final Set<String> keys1 = jedis.keys("*");
+        System.out.println(keys1 + "~~~");
+    }
+
+}
